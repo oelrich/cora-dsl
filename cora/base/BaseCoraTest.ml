b81d310d5354a8ac522d7953b979612316620f4d
Johan Oelrich <johan@oelrich.se>
2018-04-15 15:25:58 +0200

    Added larger read/write-test.

--- cora/base/BaseCoraTest.ml
+++ cora/base/BaseCoraTest.ml
@@ -1,1 +1,11 @@
+open CoraJson
+open CoraCompare
 
+let%test "compare =" =
+    let base_file = "../../test_data/test.json" in
+    let base = CoraJson.cora_of_json base_file in
+    let save_file = "./tmp.json" in
+    let reload =
+        CoraJson.cora_to_json base save_file;
+        CoraJson.cora_of_json save_file in
+    (compare base reload) = 0
